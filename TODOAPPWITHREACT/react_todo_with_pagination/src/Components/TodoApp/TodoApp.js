3cb8ddcd2b965e5b18a28308bcbc87b31b925081
Abhisek753 <properachievement@gmail.com>
2023-03-26 15:43:23 +0530

    react todo

--- TODOAPPWITHREACT/react_todo_with_pagination/src/Components/TodoApp/TodoApp.js
+++ TODOAPPWITHREACT/react_todo_with_pagination/src/Components/TodoApp/TodoApp.js
@@ -19,18 +19,33 @@ const TodoApp = () => {
     setInput("")
   }
     
-  
-
+  //delete and toggle
+  const handleDelete=(id)=>{
+   let filtertodo=todo.filter((el)=>{
+       return el.id!=id
+   })
+    setTodo(filtertodo)
+    console.log("delete")
+  }
+  const handleToggle=(id)=>{
+    console.log("toggle",id)
+     const afterupdateTodo=todo.map((ele)=>{
+      return ele.id===id?{...ele,status:!ele.status}:ele;
+     })
+    setTodo(afterupdateTodo)
+  }
   console.log(todo)
+
+
   return (
-    <div >
-      <div style={{display:"flex",marginLeft:"30%"}}>
-      <input placeholder='fill the fields' value={input} onChange={handleChange} />
+    <div style={{display:"flex",width:"70%",alignItems:"center",flexDirection:"column",border:"1px solid red",margin:"auto"}}>
+      <div style={{display:"flex",}}>
+      <input  style={{width:"300px",height:"40px",fontSize:"large"}} placeholder='fill the fields' value={input} onChange={handleChange} />
       <button onClick={handleSubmit} >Add</button>
       </div>
-      <div>
+      <div >
         {todo.map((e)=>(
-         <TodoAppList {...e}/>
+         <TodoAppList {...e} handleDelete={handleDelete} handleToggle={handleToggle} />
             
         ))}
       </div>
--- TODOAPPWITHREACT/react_todo_with_pagination/src/Components/TodoApp/TodoAppList.js
+++ TODOAPPWITHREACT/react_todo_with_pagination/src/Components/TodoApp/TodoAppList.js
@@ -1,26 +1,18 @@
 import React from 'react'
 
-const TodoAppList = (e) => {
-  const handleDelete=(id)=>{
-    e.filter((id)=>{
-      return id!=e.id
-    })
-    console.log("delete")
-  }
-  const handleToggle=(e)=>{
-    console.log("toggle",e.id)
-     
+const TodoAppList = ({title,id,status,handleDelete,handleToggle}) => {
+//all item id
+// console.log(id)
 
-  }
   return (
     <div>
-       <div key={e.id} style={{display:"flex",border:"2px solid green",alignItems:"center"}}>
+       <div key={id} style={{display:"flex",alignItems:"center"}}>
             <div style={{display:"flex",marginRight:"10px",alignItems:"center"}} >
-               <h1>{e.title}</h1>
-               <h4 style={{marginLeft:"10px"}}>{e.status?"Completed":"Not Completed"}</h4>
+               <h1 style={{color:"red"}}>{title}</h1>
+               <h4 style={{marginLeft:"10px"}}>{status?"Completed":"Not Completed"}</h4>
             </div>
-        <button onClick={()=>handleToggle(e)} >Toggle</button>
-        <button onClick={()=>handleDelete(e)} >Delete</button>
+        <button onClick={()=>handleToggle(id)} >Toggle</button>
+        <button onClick={()=>handleDelete(id)} >Delete</button>
 
         </div>
     </div>
