7df882619861486dbbe4741bee4a85e9e2c4b2b8
Abhisek753 <properachievement@gmail.com>
2023-05-08 14:19:04 +0530

    start mongoose2

--- Revise_Backend_S3/Mongooses1d1/db.js
+++ Revise_Backend_S3/Mongooses1d1/db.js
@@ -1,7 +1,14 @@
 const mongoose=require("mongoose")
 
-const main=()=>{
-  const connection=mongoose.connect("mongodb://127.0.0.1:27017")
-  console.log("connected")
+const main=async()=>{
+    try{
+        const connection=await mongoose.connect("mongodb://127.0.0.1:27017")
+        console.log("connected")
+        connection.disconnect()
+        console.log("disconnected")
+    }catch(err){
+        console.log(err)
+    }
+ 
 }
 main()
